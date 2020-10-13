91bc1da17f85a831c8fa1854bd96bcaf402538a8
Thornierlemur <70161879+Thornierlemur@users.noreply.github.com>
2020-10-12 21:26:43 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Jaime Garcia, Rodrigo Palacios, Isaac Valdez
+# 10/12/2020
+# AlgoViz
+# Description:
+# This program is meant to visually show the speed at which sorting algorithms operate at.
+# In the program the user will be able to select from a variety of sorting algorithms and test their speeds!
+# Users will also get detailed information about each algorithm and how it operates
+
+import tkinter as tk
