35e763b432383c046579cb5cfe1d5854b2d7b72e
Jaime Garcia Jr <jjgarcia2725@gmail.com>
2020-10-23 17:48:26 -0500

    erased unecessary comments

--- main.py
+++ main.py
@@ -47,24 +47,19 @@ fontStyle3 = tkFont.Font(family="Lucida Grande", size=15)
 # USER INTERFACE
 f = Frame(w, background=GRAY)
 title = tk.Label(f, text="AlgoViz", background=GRAY, font=fontStyle1)
-# title.place(x=300, y=200)
 
 selection = tk.Label(f, text="Algorithms", background=GRAY, font=fontStyle2)
-# selection.place(x=310, y=275)
 
 bubble_button = tk.Button(f, text="Bubble Sort", borderwidth=1,
                           width=12, height=1, relief="solid", bg=GRAY, font=fontStyle3)
-# bubble_button.place(x=150, y=325)
 bubble_button.bind("<Button 1>", bubble_page)
 
 insertion_button = tk.Button(f, text="Insertion Sort", borderwidth=1,
                              width=12, height=1, relief="solid", bg=GRAY, font=fontStyle3)
-# insertion_button.place(x=300, y=325)
 insertion_button.bind("<Button 1>", insertion_page)
 
 merge_button = tk.Button(f, text="Merge Sort", borderwidth=1,
                          width=12, height=1, relief="solid", bg=GRAY, font=fontStyle3)
-# merge_button.place(x=450, y=325)
 merge_button.bind("<Button 1>", merge_page)
 
 title.grid(row=0, column=1)
@@ -72,8 +67,6 @@ selection.grid(row=1, column=1)
 bubble_button.grid(row=2, column=0, padx=5, pady=5)
 insertion_button.grid(row=2, column=1, padx=5, pady=5)
 merge_button.grid(row=2, column=2, padx=5, pady=5)
-
-# This is a test comment
 
 f.pack(expand=True)
 w.mainloop()
