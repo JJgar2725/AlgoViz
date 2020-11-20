b41402a5ab31270768461d17450d404cf3e3dd9c
Jaime Garcia Jr <jjgarcia2725@gmail.com>
2020-11-20 16:24:37 -0600

    fixed coloring on selection sort

--- selectionsort.py
+++ selectionsort.py
@@ -10,13 +10,19 @@ def selection_sort(arr, draw_data, canvas, sort, count, i_count):
         for j in range(i+1, len(arr)):
             count += 1
             i_count["text"] = f"{count}" # I put the count and update of count in the for loop since this is where the checks are happening
+            
             if arr[min_idx] > arr[j]: 
                 min_idx = j
+                draw_data(arr, canvas, sort, ['green' if x == j or x == min_idx else 'blue' for x in range(len(arr))])
+                time.sleep(0.2)
+            else:
+                draw_data(arr, canvas, sort, ['red' if x == j else 'blue' for x in range(len(arr))])
+                time.sleep(0.2)
+
               
         # Swap the found minimum element with  
         # the first element         
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        
-        draw_data(arr, canvas, sort, ['red' if x == j or x == j + 1 else 'blue' for x in range(len(arr))])
+        draw_data(arr, canvas, sort, ['red' if x == i or x == min_idx else 'blue' for x in range(len(arr))])
         time.sleep(0.2)
     draw_data(arr, canvas, sort, ['red' for x in range(len(arr))])
